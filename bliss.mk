da612501a1d31bb8989268e370185fb33a8aa865
Nathan Prevette <nprevette420@gmail.com>
2015-07-12 10:15:57 -0400

    Build Viper not AudioFX

--- bliss.mk
+++ bliss.mk
@@ -14,6 +14,9 @@ TARGET_SCREEN_HEIGHT := 1280
 PRODUCT_COPY_FILES += \
     device/motorola/moto_msm8960_jbbl/config/media_codecs.xml:system/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+    
+#Bliss Viper Audio Mod
+BLISS_AUDIO_MOD := viper
 
 # Release name
 PRODUCT_NAME := bliss_moto_msm8960_jbbl
