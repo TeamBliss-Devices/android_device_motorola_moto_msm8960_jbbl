8b9aa93d17ac0cf397f139a45d2f7496ccd7a666
Nathan Prevette <nprevette420@gmail.com>
2015-07-30 23:17:43 -0400

    Set to permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_USES_LEGACY_MMAP := true
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8960-common
 TARGET_KERNEL_CONFIG := msm8960_mmi_defconfig
-BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 zcache=lz4
+BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 zcache=lz4 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
