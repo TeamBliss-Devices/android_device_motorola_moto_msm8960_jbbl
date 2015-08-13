3604892186ca1ea69bb73e0d5145681c69ebc8dc
Nathan Prevette <nprevette420@gmail.com>
2015-08-12 17:48:41 -0700

    Hopefully fix sound

--- device_moto_msm8960.mk
+++ device_moto_msm8960.mk
@@ -104,7 +104,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/v4a/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/v4a/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/v4a/vendor/etc/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/config/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
     $(LOCAL_PATH)/config/snd_soc_msm_2x_xt92x:system/etc/snd_soc_msm/snd_soc_msm_2x_xt92x
 
